e755199341d4602ee9989d301276b79a794a15b1
Yu Jiawei <jiawei.yu@uq.edu.au>
2024-12-10 15:59:49 +1000

    Update setupVdjPseudobulk.R

--- R/setupVdjPseudobulk.R
+++ R/setupVdjPseudobulk.R
@@ -253,6 +253,32 @@ setupVdjPseudobulk <- function(
     } else {
         msg <- paste(extract_cols, collapse = ", ")
         message(sprintf("Extract main TCR from %s ...", msg), appendLF = FALSE)
+        if(!all(extract_cols%in%colnames(colData(sce)))){
+          splited_TCR <- splitCTgene(sce)
+          if(length(splited_TCR[[1]])!=length(extract_cols))
+          {
+            abort(paste("Keyerror: Automatically generated colnames",
+                        paste0(extract_cols[!extract_cols%in%colnames(colData(sce))],
+                               collapse = ", "), "are with the same length of the vdj data, which is of the length", 
+                        length(splited_TCR[[1]]),
+                        "\nYou could modify parameter extract_cols to specify the columns to match the length"))
+          }
+          else
+          {
+            vdj <- lapply(seq(length(extract_cols)), function(X, sc) {
+              vapply(X = sc, '[',X, FUN.VALUE = character(1))
+            }, sc = splited_TCR)
+            names(vdj) <- extract_cols
+            colData(sce) <- cbind(colData(sce), vdj)
+          }
+        }
+        else if(any(extract_cols%in%colnames(colData(sce))))
+        {
+          abort(paste("Keyerror: Automatically generated colnames",
+                      paste0(extract_cols[!extract_cols%in%colnames(colData(sce))],
+                             collapse = ", "), "are not contained in colData",
+                      "\nYou could use parameter extract_cols to specify the columns to extract TCR"))
+        }
         sce <- Reduce(function(data, ex_col) {
             tem <- colData(data)[[ex_col]]
             strtem <- strsplit(as.character(tem), "\\|")
