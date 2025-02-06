783b75e54f18f8d259a0341ad7f064068f056dc7
Yu Jiawei <jiawei.yu@uq.edu.au>
2025-02-06 17:46:27 +1000

    formatting construct MarkovChain

--- R/constructMarkovChain.R
+++ R/constructMarkovChain.R
@@ -30,18 +30,17 @@
     })
     dist_sort <- lapply(dist_, sort, decreasing = TRUE)
     adaptive.std <- vapply(dist_sort, "[", adaptive.k, FUN.VALUE = double(1))
-    # Directed graph construction pseudotime position of all the neighbors
+    # Directed graph construction pseudotime position of all the
+    # neighbors
     traj_nbrs <- lapply(ind, function(x) {
         pseudotime[x]
     })
-    ## Remove edges that move backwards in pseudotime except for edges that are
-    ## within the computed standard deviation
-    rem_edges <- pmap(
-        list(.x = traj_nbrs, .y = (pseudotime - adaptive.std), .z = ind),
-        function(.x, .y, .z) {
-            .z[.x < .y]
-        }
-    )
+    ## Remove edges that move backwards in pseudotime except for
+    ## edges that are within the computed standard deviation
+    rem_edges <- pmap(list(.x = traj_nbrs, .y = (pseudotime - adaptive.std),
+                           .z = ind), function(.x, .y, .z) {
+                             .z[.x < .y]
+                             })
     for (i in seq_len(length(waypoints))) {
         if (length(KNN[i, rem_edges[[i]]])) {
             KNN[i, rem_edges[[i]]] <- 0
@@ -51,20 +50,19 @@
     cell_mapping <- seq_len(length(waypoints))
     ids <- summary(KNN)
     # anisotropic Diffusion Kernel
-    aff <- exp(-(ids$x^2) / (adaptive.std[ids$i]^2) * 0.5 - (ids$x^2) / (adaptive.std[ids$j]^2) *
-        0.5)
-    W <- sparseMatrix(i = ids$i, j = ids$j, x = aff, dims = dim(KNN), giveCsparse = TRUE)
+    aff <- exp(-(ids$x^2) / (adaptive.std[ids$i]^2) 
+               * 0.5 - (ids$x^2) / (adaptive.std[ids$j]^2) * 0.5)
+    W <- Matrix::sparseMatrix(i = ids$i, j = ids$j, x = aff,
+                              dims = dim(KNN), giveCsparse = TRUE)
     # Transition matrix
     D <- apply(W, 1, sum)
-    ids <- summary(W)
-    T_ <- sparseMatrix(
-        i = ids$i, j = ids$j, x = ids$x / D[ids$i], dims = dim(KNN),
-        giveCsparse = TRUE
-    )
+    ids <- Matrix::summary(W)
+    T_ <- sparseMatrix(i = ids$i, j = ids$j, x = ids$x / D[ids$i],
+                       dims = dim(KNN), giveCsparse = TRUE)
     return(T_)
 }
 
-#' Calculate the weight adjacent matricks of knn graph and its index 
+#' Calculate the weight adjacent matricks of knn graph and its index
 #' @param wp_data Multi scale data of the waypoints
 #' @param knn. Number of nearest neighbors for graph construction
 #' @keywords internal
@@ -72,17 +70,18 @@
 #' @importFrom igraph ends E<- E as_adjacency_matrix
 #' @importFrom stats dist
 #' @return a list containing the weight adjacent matrix and index
-.KNNind <- function(wp_data, knn.){
+.KNNind <- function(wp_data, knn.) {
     nbrs <- makeKNNGraph(wp_data, k = knn.)
     ## calculate distance of each edge
     distance_m <- as.matrix(dist(wp_data))
     edge_indices <- ends(nbrs, igraph::E(nbrs))
     weights <- vapply(seq_len(nrow(edge_indices)), function(i) {
-      ### Get the nodes connected by each edge
-      node1 <- as.numeric(edge_indices[i, 1])
-      node2 <- as.numeric(edge_indices[i, 2])
-      ### Use the distance matrix to get the distance between the two nodes
-      distance_m[node1, node2]
+        ### Get the nodes connected by each edge
+        node1 <- as.numeric(edge_indices[i, 1])
+        node2 <- as.numeric(edge_indices[i, 2])
+        ### Use the distance matrix to get the distance between the
+        ### two nodes
+        distance_m[node1, node2]
     }, numeric(1))
     E(nbrs)$weight <- weights
     ## generate weighted adjacent matrix of this knn graph
@@ -91,7 +90,7 @@
     idx <- KNN@p
     idx_seq <- mapply(seq, (idx + 1)[-length(idx)], idx[-1])
     ind <- lapply(idx_seq, function(x) {
-      KNN@i[x] + 1
+        KNN@i[x] + 1
     })
-    return(list(KNN=KNN, ind = ind, idx_seq = idx_seq))
+    return(list(KNN = KNN, ind = ind, idx_seq = idx_seq))
 }
